5baf6967051ddc834bbf51d4a999431c22f68d4e
choiyusung <chldbtjd2272>
2018-11-26 11:06:28 +0900

    hotfix -> 리뷰 모아보기 simpleReviewDto 어노테이션 추가

--- src/main/java/com/walkd/dmzing/dto/review/SimpleReviewDto.java
+++ src/main/java/com/walkd/dmzing/dto/review/SimpleReviewDto.java
@@ -2,7 +2,14 @@ package com.walkd.dmzing.dto.review;
 
 import com.walkd.dmzing.domain.Review;
 import io.swagger.annotations.ApiModelProperty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class SimpleReviewDto {
     @ApiModelProperty(hidden = true)
     private Long id;
